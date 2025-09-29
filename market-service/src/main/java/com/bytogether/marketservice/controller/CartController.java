fd46a37869de1d725cd2e99d092554cbbed17ffc
k1ng1nsu9805 <insu9058@naver.com>
2025-09-29 17:39:13 +0900

    ✨ feat: Add carts related class
    - Add controller
    - Add Facade Service

---
+++ market-service/src/main/java/com/bytogether/marketservice/controller/CartController.java
@@ -0,0 +1,21 @@
+package com.bytogether.marketservice.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 공동 구매 찜하기 관련 컨트롤러
+ *
+ * @author insu9058@naver.com
+ * @version 1.0
+ * @since 2025-09-29
+ */
+
+@RestController
+@RequestMapping("/api/v1/market")
+@RequiredArgsConstructor
+public class CartController {
+
+}
---
+++ market-service/src/main/java/com/bytogether/marketservice/service/CartFacadeService.java
@@ -0,0 +1,20 @@
+package com.bytogether.marketservice.service;
+
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * 공동 구매 찜하기 관련 서비스
+ *
+ * @author insu9058@naver.com
+ * @version 1.0
+ * @since 2025-09-29
+ */
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartFacadeService {
+}
