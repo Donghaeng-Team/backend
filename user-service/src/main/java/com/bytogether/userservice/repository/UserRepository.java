d8fd103c44f54e6e855a82ee5551dd95eb15883e
ellanelee <ljedoc@naver.com>
2025-10-01 08:45:53 +0900

    :sparkles: feat: UserModule_userRepository구성

--- user-service/src/main/java/com/bytogether/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/bytogether/userservice/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.bytogether.userservice.repository;
 
-public class UserRepository {
+import com.bytogether.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findById(Long id);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname) ;
 }
