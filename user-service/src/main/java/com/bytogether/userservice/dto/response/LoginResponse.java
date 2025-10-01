f6f423ee0d5995aeda91801c7e9c4b32c4a18987
ellanelee <ljedoc@naver.com>
2025-10-01 18:03:03 +0900

    :zap: perf: Login Response의 stackOverflow 방지를 위한 순환 제거

--- user-service/src/main/java/com/bytogether/userservice/dto/response/LoginResponse.java
+++ user-service/src/main/java/com/bytogether/userservice/dto/response/LoginResponse.java
@@ -3,17 +3,10 @@ package com.bytogether.userservice.dto.response;
 import lombok.*;
 
 @Getter
-@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class LoginResponse {
     private String accessToken;
     private String refreshToken;
-
-    public LoginResponse(String accessToken, String refreshToken) {
-        LoginResponse.builder()
-                .accessToken(accessToken)
-                .refreshToken(refreshToken)
-                .build();
-    }
 }
