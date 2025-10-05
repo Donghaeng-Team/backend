119a840b774d5d327dda39e412157f6bf9da310f
latte3cup <latte3cup@naver.com>
2025-10-05 20:33:59 +0900

    feat : add 2 repositories, post and postStat
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- comm-service/src/main/java/com/bytogether/commservice/repository/PostRepository.java
+++ comm-service/src/main/java/com/bytogether/commservice/repository/PostRepository.java
@@ -5,6 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Page<Post> findByRegion(Pageable pageable);
+
+    Optional<Post> findByPostIdAndDeletedIsFalse(Long postId);
+
+    List<Post> findByAuthorIdAndDeletedIsFalse(Long userId, Pageable pageable);
 }
