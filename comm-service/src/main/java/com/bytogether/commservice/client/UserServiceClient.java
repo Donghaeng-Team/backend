938cd436052be6f9abd00f3395e92abcc50f0aa1
latte3cup <latte3cup@naver.com>
2025-10-05 12:29:25 +0900

    feat : add feign Client to user-service
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- comm-service/src/main/java/com/bytogether/commservice/client/UserServiceClient.java
+++ comm-service/src/main/java/com/bytogether/commservice/client/UserServiceClient.java
@@ -1,12 +1,13 @@
 package com.bytogether.commservice.client;
 
 import com.bytogether.commservice.client.dto.UserDto;
+import com.bytogether.commservice.dto.ApiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
-    @GetMapping("/users/{id}")
-    UserDto getUserById(@PathVariable("id") Long id);
+    @GetMapping("/api/v1/user/private/userInfo")
+    ApiResponse<UserDto> getUserInfo(@RequestParam("userId") Long userId);
 }
