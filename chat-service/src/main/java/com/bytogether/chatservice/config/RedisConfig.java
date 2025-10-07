bc81b0416e0c26252c3b98f6e360677898907c39
jhj010311 <jhj010311@gmail.com>
2025-10-07 14:45:18 +0900

    ✨ feat: Add configs
    - Redis, WebSocket configs

--- chat-service/src/main/java/com/bytogether/chatservice/config/RedisConfig.java
+++ chat-service/src/main/java/com/bytogether/chatservice/config/RedisConfig.java
@@ -1,4 +1,37 @@
 package com.bytogether.chatservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+
+/**
+ * Redis 설정
+ *
+ * @author jhj010311@gmail.com
+ * @version 1.0
+ * @since 2025-10-07
+ */
+
+@Configuration
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
 }
