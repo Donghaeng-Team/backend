4633b812ac69b5d3c11ebf042464c3684481ac03
latte3cup <latte3cup@naver.com>
2025-10-12 00:03:13 +0900

    feat: add CommentsRepository
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- comm-service/src/main/java/com/bytogether/commservice/repository/CommentRepository.java
+++ comm-service/src/main/java/com/bytogether/commservice/repository/CommentRepository.java
@@ -1,11 +1,10 @@
 package com.bytogether.commservice.repository;
 
 import com.bytogether.commservice.entity.Comment;
-import com.bytogether.commservice.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    Page<Comment> findByPost(Post post, Pageable pageable);
+    Page<Comment> findByPost_PostIdOrderByCreatedAtAsc(Long postId, Pageable pageable);
 }
