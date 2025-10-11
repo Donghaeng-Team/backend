75bcc2cb533ae15609c5a0580543bee9d6b56ef4
latte3cup <latte3cup@naver.com>
2025-10-12 05:19:08 +0900

    feat : 상태 열거형 추가로 인한 status 필드 추가 (post table) - 상태 다양화로 인한 삭제 시간(deleted_at) 추가
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- comm-service/src/main/java/com/bytogether/commservice/entity/Post.java
+++ comm-service/src/main/java/com/bytogether/commservice/entity/Post.java
@@ -1,5 +1,6 @@
 package com.bytogether.commservice.entity;
 
+import com.bytogether.commservice.dto.Enum.PostStatus;
 import com.bytogether.commservice.util.JsonStringListConverter;
 import jakarta.persistence.*;
 import lombok.*;
@@ -16,8 +17,8 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@Where(clause = "deleted=false")
-@SQLDelete(sql = "UPDATE posts SET deleted = true WHERE post_id = ?")
+@Where(clause = "status = 'PUBLISHED'")
+@SQLDelete(sql = "UPDATE posts SET deleted_at = now(), status = 'DELETED' WHERE post_id = ?")
 @Table(name = "posts")
 public class Post {
     @Id
@@ -30,7 +31,7 @@ public class Post {
     @Column(nullable = false)
     private String tag;
 
-    @Column(nullable = false)
+    @Column(nullable = false,name="author_id")
     private Long authorId;
 
     @Column(nullable = false, length = 200)
@@ -39,50 +40,36 @@ public class Post {
     @Column(nullable= false, columnDefinition = "text")
     private String content;
 
-    @Column(nullable = false,updatable = false)
+    @Column(nullable = false,updatable = false,name = "created_at")
     private LocalDateTime createdAt;
 
-    @Column
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @Builder.Default
-    @Column(nullable = false)
-    private boolean deleted = false;
-
-    @Builder.Default
-    @OneToMany(mappedBy = "post")
-    private List<Comment> comments = new ArrayList<>();
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 
     @Column(name = "thumbnail_url")
     private String thumbnailUrl;
-
 
     @Convert(converter = JsonStringListConverter.class)
     @Column(name = "image_urls", columnDefinition = "TEXT")
     private List<String> imageUrls;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private PostStatus status = PostStatus.TEMP;
 
+
+    @Builder.Default
+    @OneToMany(mappedBy = "post")
+    private List<Comment> comments = new ArrayList<>();
 
     @PrePersist
     public void onCreate() {
         if (this.createdAt == null) this.createdAt = LocalDateTime.now();
-        updateThumbnail();
     }
 
-    @PreUpdate
-    public void onUpdate() {
-        this.updatedAt = LocalDateTime.now();
-        updateThumbnail();
-    }
-
-    // 썸네일 로직을 처리하는 private 헬퍼 메서드
-    private void updateThumbnail() {
-        if (this.imageUrls != null && !this.imageUrls.isEmpty()) {
-            this.thumbnailUrl = this.imageUrls.get(0);
-        } else {
-            this.thumbnailUrl = null;
-        }
-    }
 
 
 
