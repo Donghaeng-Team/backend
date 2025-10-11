1371a665d5f425c705d44275e35f15822ab853ad
latte3cup <latte3cup@naver.com>
2025-10-12 05:17:47 +0900

    feat : 게시물 상태에 대해 더 넓은 범위 적용을 위한 enum 추가
    Signed-off-by: latte3cup <latte3cup@naver.com>

---
+++ comm-service/src/main/java/com/bytogether/commservice/dto/Enum/PostStatus.java
@@ -0,0 +1,8 @@
+package com.bytogether.commservice.dto.Enum;
+
+public enum PostStatus {
+    TEMP,        // 임시 저장 (작성 중)
+    PUBLISHED,   // 공개됨
+    HIDDEN,      // 숨김
+    DELETED      // 삭제됨
+}
