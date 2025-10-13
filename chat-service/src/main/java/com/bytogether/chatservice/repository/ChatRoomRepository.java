115d9b81fecbbf847d13d52641534ac75f84a1fb
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-10-13 16:32:58 +0900

    ♻️ refactor: ChatRoomRepository 현재 불필요한 코드 제거

--- chat-service/src/main/java/com/bytogether/chatservice/repository/ChatRoomRepository.java
+++ chat-service/src/main/java/com/bytogether/chatservice/repository/ChatRoomRepository.java
@@ -1,6 +1,8 @@
 package com.bytogether.chatservice.repository;
 
 import com.bytogether.chatservice.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -13,19 +15,11 @@ import java.util.List;
  * 채팅방 엔티티에 대한 데이터베이스 접근을 담당하는 레포지토리
  *
  * @author jhj010311@gmail.com
- * @version 1.0
- * @since 2025-10-07
+ * @version 1.01
+ * @since 2025-10-13
  */
 
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 
-    List<ChatRoom> findByMarketId(Long marketId);
-
-    List<ChatRoom> findByStatus(ChatRoomStatus status);
-
-    List<ChatRoom> findByCreatorUserId(Long userId);
-
-    @Query("SELECT cr FROM ChatRoom cr WHERE cr.endTime < :now AND cr.status = 'RECRUITING'")
-    List<ChatRoom> findExpiredRecruitingRooms(@Param("now") LocalDateTime now);
 }
