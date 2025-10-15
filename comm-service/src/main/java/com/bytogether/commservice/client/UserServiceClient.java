a7a791268877ccacb453e5085b1a289ed4b83f6d
latte3cup <latte3cup@naver.com>
2025-10-15 14:30:05 +0900

    fix : UserServiceClient가 user-service에 맞게 수정
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- comm-service/src/main/java/com/bytogether/commservice/client/UserServiceClient.java
+++ comm-service/src/main/java/com/bytogether/commservice/client/UserServiceClient.java
@@ -1,13 +1,15 @@
 package com.bytogether.commservice.client;
 
 import com.bytogether.commservice.client.dto.UserDto;
-import com.bytogether.commservice.dto.ApiResponse;
+import com.bytogether.commservice.client.dto.UserInternalResponse;
+import com.bytogether.commservice.client.dto.UsersInfoRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
 
 @FeignClient(name = "user-service")
 public interface UserServiceClient {
-    @GetMapping("/api/v1/user/private/userInfo")
-    ApiResponse<UserDto> getUserInfo(@RequestParam("userId") Long userId);
+    @GetMapping("/internal/v1/user/userinfo")
+    List<UserInternalResponse> getUserInfo(@RequestBody UsersInfoRequest userId);
 }
