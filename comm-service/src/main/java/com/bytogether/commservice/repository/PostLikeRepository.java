d5e5ab825a6e5660b9ac336e9ccb84614f9db712
latte3cup <latte3cup@naver.com>
2025-10-16 12:38:31 +0900

    chore: 사용하지 않는 메서드 삭제
    Signed-off-by: latte3cup <latte3cup@naver.com>

--- comm-service/src/main/java/com/bytogether/commservice/repository/PostLikeRepository.java
+++ comm-service/src/main/java/com/bytogether/commservice/repository/PostLikeRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
     boolean existsByPostIdAndUserId(Long postId, Long userId);
-    void deleteByPostIdAndUserId(Long postId, Long userId);
 }
