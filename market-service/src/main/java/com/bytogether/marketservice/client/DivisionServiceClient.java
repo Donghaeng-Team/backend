c5b0bde29075b2fbccc9591ea720fa21d421fca4
k1ng1nsu9805 <insu9058@naver.com>
2025-10-20 15:47:20 +0900

    ✨ feat: market eks에 맞게

--- market-service/src/main/java/com/bytogether/marketservice/client/DivisionServiceClient.java
+++ market-service/src/main/java/com/bytogether/marketservice/client/DivisionServiceClient.java
@@ -16,7 +16,7 @@ import java.util.Optional;
  * @since 2025-09-31
  */
 
-@FeignClient(name = "division-service")
+@FeignClient(name = "division-service",url = "${openfeign.division-service.url}")
 public interface DivisionServiceClient {
     // 좌표 기반 행정구역 조회
     @GetMapping("/api/v1/division/public/by-coord")
