0c4d18c3310f653d45059a4d7d835ac478006c4c
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-10-23 16:23:54 +0900

    ✨ feat: 채팅방 및 참가자 상태 관리 enum 최적화
    - 설명 구체화 및 필요한 경우 분화

--- chat-service/src/main/java/com/bytogether/chatservice/entity/ParticipantStatus.java
+++ chat-service/src/main/java/com/bytogether/chatservice/entity/ParticipantStatus.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum ParticipantStatus {
     ACTIVE("활동중"),
     LEFT_VOLUNTARY("자발적 탈퇴"),
-    LEFT_NOT_BUYER("미참여자 자동퇴장"),
+    LEFT_RECRUITMENT_CLOSED("모집마감 자동퇴장"),  // 구매자가 아닌 사람 퇴장
+    LEFT_RECRUITMENT_CANCELED("채팅방 종료"),                // 방장이 채팅방 종료
     LEFT_COMPLETED("정상종료"),
     BANNED("강퇴");
 
