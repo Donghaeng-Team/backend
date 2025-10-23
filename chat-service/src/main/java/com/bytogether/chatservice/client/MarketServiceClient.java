eac46289e18bdd2611ac4598ee4d675a9a735c17
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-10-23 16:25:02 +0900

    ✨ feat: 채팅방 상태변경시 market-service에 연동되어 반영하도록 요청하는 기능 추가
    - chat-service에서 방장인지 검증 후 방장의 요청이 맞으면 market-service 대응 FeignClient를 호출하여 요청

---
+++ chat-service/src/main/java/com/bytogether/chatservice/client/MarketServiceClient.java
@@ -0,0 +1,30 @@
+package com.bytogether.chatservice.client;
+
+import com.bytogether.chatservice.client.dto.UserInternalResponse;
+import com.bytogether.chatservice.client.dto.UsersInfoRequest;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * Market Service와 통신하기 위한 Feign Client
+ *
+ * @author jhj010311@gmail.com
+ * @version 1.0
+ * @since 2025-10-23
+ *
+ */
+
+@FeignClient(name = "market-service")
+public interface MarketServiceClient {
+
+    @GetMapping("/internal/v1/market/status/cancel/{marketId}")
+    void cancelMarketPost(@PathVariable Long marketId, Long requestUserId);
+
+    @GetMapping("/internal/v1/market/status/complete/{marketId}")
+    void completeMarketPost(@PathVariable Long marketId, Long requestUserId);
+}
