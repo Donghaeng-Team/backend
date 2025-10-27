ec0d3a5aac7671a27669decfdcd41be70fb6f89a
DESKTOP-GR883KV\user <jhj010311@gmail.com>
2025-10-27 19:18:41 +0900

    🐛 fix: 시스템 메시지 발송시 닉네임 대신 유저id가 노출되던 부분 수정

--- chat-service/src/main/java/com/bytogether/chatservice/service/ChatRoomService.java
+++ chat-service/src/main/java/com/bytogether/chatservice/service/ChatRoomService.java
@@ -354,7 +354,9 @@ public class ChatRoomService {
         // ChatRoomParticipantHistory 테이블에 기록
         historyRepository.updateLeftAtAndExitType(saved.getChatRoom().getId(), userId, saved.getLeftAt(), ExitType.VOLUNTARY);
 
-        String system = userId.toString() + "님이 퇴장하셨습니다";
+        UserInternalResponse userInfo = userServiceClient.getUserInfo(UserInfoRequest.builder().userId(userId).build());
+
+        String system = userInfo.getNickName() + "님이 퇴장하셨습니다";
 
         chatMessageService.sendSystemMessage(roomId, system);
 
@@ -398,7 +400,9 @@ public class ChatRoomService {
 
         int currentBuyers = participantRepository.countBuyersByRoomId(roomId);
 
-        String system = userId.toString() + "님이 공동구매에 참가하셨습니다\n현재 구매자 : " + currentBuyers;
+        UserInternalResponse userInfo = userServiceClient.getUserInfo(UserInfoRequest.builder().userId(userId).build());
+
+        String system = userInfo.getNickName() + "님이 공동구매에 참가하셨습니다\n현재 구매자 : " + currentBuyers;
 
         chatMessageService.sendSystemMessage(roomId, system);
 
@@ -423,7 +427,9 @@ public class ChatRoomService {
 
         int currentBuyers = participantRepository.countBuyersByRoomId(roomId);
 
-        String system = userId.toString() + "님이 공동구매 참가를 취소하셨습니다\n현재 구매자 : " + currentBuyers;
+        UserInternalResponse userInfo = userServiceClient.getUserInfo(UserInfoRequest.builder().userId(userId).build());
+
+        String system = userInfo.getNickName() + "님이 공동구매 참가를 취소하셨습니다\n현재 구매자 : " + currentBuyers;
 
         chatMessageService.sendSystemMessage(roomId, system);
 
